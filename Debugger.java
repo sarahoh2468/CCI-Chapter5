863d6f469df9b99431f2bd416e3f9a5f1fe58b2f
sarahoh2468 <40012392+sarahoh2468@users.noreply.github.com>
2018-08-01 02:41:57 -0400

    Add files via upload

---
+++ Debugger.java
@@ -0,0 +1,4 @@
+public class Debugger {
+    // Explain what the following code does: ((n & (n-1)) == 0
+    // They have no 1s in the same place because n-1 is the exact opposite of n in terms of binary 1s and 0s
+}
